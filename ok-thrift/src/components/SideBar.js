be8caf7b69feb25d246d84c49d2eb1116416fe0b
Kade <kadeesterline@gmail.com>
2022-03-07 10:13:22 -0800

    cleaned rest of files

--- ok-thrift/src/components/SideBar.js
+++ ok-thrift/src/components/SideBar.js
@@ -1,6 +1,18 @@
 
 function SideBar(){
-    console.log("Side Bar")
+    return (
+        <div>
+            <button className='btn btn-secondary'>
+                Home
+            </button>
+            <button className='btn btn-secondary'>
+                Cart
+            </button>
+            <button className='btn btn-secondary'>
+                Profile
+            </button>
+        </div>
+    )
 }
 
 export default SideBar;
