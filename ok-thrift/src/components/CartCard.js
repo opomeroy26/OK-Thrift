30004e3e21efd477e18b8368690cb8f613df839c
Olivia Pomeroy <opomeroy26@gmail.com>
2022-03-08 15:55:03 -0800

    Detail Page and Card

--- ok-thrift/src/components/CartCard.js
+++ ok-thrift/src/components/CartCard.js
@@ -1,16 +1,16 @@
 import React from 'react'
 import '../styles/CartCard.css'
-function CartCard({product, handleRemoveFromCart, handleCardClick}) {
+function CartCard({product, handleRemoveFromCart}) {
     const {image, name, description, price} = product
     
     return (
         <div>
-            <div onClick={()=> handleCardClick(product)} className="card col-12  p-2 m-2" >
+            <div className= "card col-12  p-2 m-2" >
                 <img className="card-img-top image" src={image} alt={name}/>
                 <div className="card-body h-100">
                     <h5 className="card-title">{name}</h5>
-                    <p className="card-text">{description}</p>
-                    <span className="price">{price}</span>
+                    <p className="card-text-item">{description}</p>
+                    <p className="price">{price}</p>
                     <button onClick={(e)=>handleRemoveFromCart(e, product)} className="btn btn-secondary">Remove From Cart</button>
                 </div>
             </div>
