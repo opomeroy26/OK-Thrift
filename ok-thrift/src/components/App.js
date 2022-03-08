df3ab29d8245d21cbde56dead3ed3378a321685c
Olivia Pomeroy <opomeroy26@gmail.com>
2022-03-08 08:40:19 -0800

    Rendered Item Cards to Home Page

--- ok-thrift/src/components/App.js
+++ ok-thrift/src/components/App.js
@@ -1,4 +1,5 @@
 import {Switch, Route} from "react-router-dom"
+import React, {useEffect, useState} from "react";
 //import Header from "./Header";
 import SideBar from "./SideBar";
 import HomePage from "./HomePage";
@@ -8,6 +9,14 @@ import ProfilePage from "./ProfilePage";
 import '../styles/index.css'
 
 function App() {
+  const [inventory, setInventory] = useState([])
+
+  useEffect(()=> {
+    fetch("http://localhost:3000/inventory")
+    .then((resp) => resp.json())
+    .then(setInventory)
+  }, [])
+
   return (
     <div className="container-fluid" >
       
@@ -19,7 +28,7 @@ function App() {
       <div className='col-10 switch '>
       <Switch>
         <Route exact path="/">
-          <HomePage />
+          <HomePage inventory={inventory}/>
         </Route>
       
         <Route exact path="/cart">
--- ok-thrift/src/components/HomePage.js
+++ ok-thrift/src/components/HomePage.js
@@ -1,16 +1,18 @@
 import ItemCard from './ItemCard.js'
 
-function HomePage(){
+function HomePage({inventory}){
+    const product = inventory.map((productObj)=> (
+        <div>
+            <ItemCard 
+                key={productObj.id + productObj.name}
+                inventory={productObj}
+            />
+        </div>
+    ))
+    
     return (
         <div className='row' id='items'>
-            <ItemCard />
-            <ItemCard />
-            <ItemCard />
-            <ItemCard />
-            <ItemCard />
-            <ItemCard />
-            <ItemCard />
-            <ItemCard />
+            {product}
         </div>
     )
 }
