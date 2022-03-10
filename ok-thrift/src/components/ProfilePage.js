2308f3b7995dfacd2cf2b9d0de84afc8eb68dfc5
Olivia Pomeroy <opomeroy26@gmail.com>
2022-03-09 21:12:52 -0800

    Cleaning up some code

--- ok-thrift/src/components/ProfilePage.js
+++ ok-thrift/src/components/ProfilePage.js
@@ -1,6 +1,5 @@
 import { useState } from 'react'
 import '../styles/ProfilePage.css'
-import ItemCard from './ItemCard.js'
 import Form from './Form.js'
 import ProfileItemCard from './ProfileItemCard'
 
